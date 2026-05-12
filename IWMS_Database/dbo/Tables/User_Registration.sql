d78c883b4bde25a59be90d811af0f1db4b49b3b4
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2026-05-12 07:14:54 +0600

    Initial Database Project Import

---
+++ IWMS_Database/dbo/Tables/User_Registration.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User_Registration] (
+    [FullName] VARCHAR (50)  NOT NULL,
+    [UserName] VARCHAR (10)  NOT NULL,
+    [Password] VARCHAR (50)  NOT NULL,
+    [Email]    VARCHAR (30)  NOT NULL,
+    [Phone]    VARCHAR (15)  NOT NULL,
+    [Address]  VARCHAR (250) NOT NULL,
+    [DOB]      DATE          NOT NULL,
+    [Gender]   VARCHAR (30)  NOT NULL,
+    [Role]     VARCHAR (30)  NOT NULL
+);
+
