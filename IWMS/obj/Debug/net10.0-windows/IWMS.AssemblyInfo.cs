e4d2f2814f9ceb282659ea0303727576836f0f38
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2026-05-18 18:08:24 +0600

    Checked all errors

--- IWMS/obj/Debug/net10.0-windows/IWMS.AssemblyInfo.cs
+++ IWMS/obj/Debug/net10.0-windows/IWMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IWMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b91afcf52b51457a06da755d9f6b046f0141e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83e46a4ff45c505258b3522090b27601755fe425")]
 [assembly: System.Reflection.AssemblyProductAttribute("IWMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IWMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
