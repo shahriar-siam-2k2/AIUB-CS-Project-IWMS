29acacfa902886b6a2bda800e999121a90129e3d
Shahriar Ali Siam <shahriaraliseam@gmail.com>
2026-05-20 02:24:15 +0600

    Fixed login button

--- IWMS/Login.Designer.cs
+++ IWMS/Login.Designer.cs
@@ -110,7 +110,7 @@
             btnLogin.BackColor = Color.FromArgb(128, 255, 128);
             btnLogin.Cursor = Cursors.Hand;
             btnLogin.Font = new Font("Trebuchet MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnLogin.Location = new Point(314, 322);
+            btnLogin.Location = new Point(175, 323);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(123, 34);
             btnLogin.TabIndex = 7;
